d1bba73d7dd1108dfae4c730bdbf174bdefe3e3b
kaspar.arme <kaspar.arme@reach-u.com>
2021-02-03 19:18:35 +0200

    Add styling and templating

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ["./**/*.html", "./**/*.pug"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {}
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
+};

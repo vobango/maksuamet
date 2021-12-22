162bb1d5aa5c4a0f330f5f2f6fea3b25ebebbd2a
Taavi Saar <taavi.saar@playtech.com>
2021-12-22 16:06:31 +0200

    gitignore fix

--- setup.sh
+++ setup.sh
@@ -1,14 +1,7 @@
 #!/usr/bin/env bash
 ## We need environmental variables in ~/.env
 
-if [ "$1" == "init" ]; then
-  git clone https://github.com/vobango/maksuamet.git
-  git clone https://github.com/vobango/maksuamet-fe.git
-  cd maksuamet || echo "Git clone failed"; exit 1 ;
-  cp ../.env ./
-  docker-compose -f docker-compose.yaml up -d
-fi;
-
+#update
 if [ "$1" == "update" ] || [ "$1" == "" ]; then
   cd ../maksuamet-fe || echo "No such dir $?"; exit 1 ;
   git pull

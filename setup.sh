c5a2c78ee6185af0b5a46b898df467d8a390a27a
Taavi Saar <taavi.saar@playtech.com>
2021-12-22 15:36:32 +0200

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/env bash
-## We need environmental variables
-cp ../.env ./
+## We need environmental variables in ~/.env
 
 if [ "$1" == "init" ]; then
   git clone https://github.com/vobango/maksuamet.git
   git clone https://github.com/vobango/maksuamet-fe.git
   cd maksuamet || echo "Git clone failed"; exit 1 ;
+  cp ../.env ./
   docker-compose -f docker-compose.yaml up -d
 fi;
 
 if [ "$1" == "update" ] || [ "$1" == "" ]; then
-  cd ..
-  git pull https://github.com/vobango/maksuamet.git
-  git pull https://github.com/vobango/maksuamet-fe.git
+  cd ../maksuamet-fe || echo "No such dir $?"; exit 1 ;
+  git pull
+  cd ../maksuamet || echo "No such dir $?"; exit 1 ;
+  git pull
+  cp ../.env ./
   docker-compose -f ./maksuamet/docker-compose.yaml up -d
 fi;
